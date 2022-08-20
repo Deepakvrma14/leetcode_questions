08feb593f6ba3c6e038c690dd952fade883e2ade
Deepakvrma14 <86903293+Deepakvrma14@users.noreply.github.com>
2022-08-20 08:08:33 +0530

    Time: 11 ms (87.75%), Space: 41.5 MB (97.20%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x==-1){
+            return false;
+        }
+        int reverse = rev(x);
+        if(x==reverse){
+            return true;
+        }
+         
+        else{
+            return false;
+        }
+    }
+    static int rev(int n){
+        int digit = (int)Math.log10(n)+1;
+        return helper(n,digit);
+        
+    }
+    static int helper(int n , int digit){
+        if(n%10==n){
+            return n;
+        }
+        int rem = n %10;
+        return rem * (int)Math.pow(10,digit-1) + helper(n/10, digit-1);
+    }
+}
